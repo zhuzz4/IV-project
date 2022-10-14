f4cbce50c494105559227ae28be3a340f7f576a8
zhuzz4 <zhuzz4@student.unimelb.edu.au>
2022-10-14 11:18:07 +1100

    weather feature and fix a bug in home page.

--- script.js
+++ script.js
@@ -38,3 +38,18 @@ $(".navbar-toggler").click(function(){
 	$(".navbar-collapse").slideToggle(300);
 	setTimeout(function(){ test(); });
 });
+
+
+jQuery(document).ready(function($){
+	// Get current path and find target link
+	var path = window.location.pathname.split("/").pop();
+
+	// Account for home page with empty path
+	if ( path == '' ) {
+		path = 'index.html';
+	}
+
+	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
+	// Add active class to target link
+	target.parent().addClass('active');
+});
