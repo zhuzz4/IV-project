38dbb85f295474fd9138b600dd514282454a51d3
zhuzz4 <111069564+zhuzz4@users.noreply.github.com>
2023-05-11 18:37:28 +1000

    Delete t_XXX directory

--- t_XXX/first_move.py
+++
@@ -1,9 +0,0 @@
-from template import Agent
-
-
-class myAgent(Agent):
-    def __init__(self,_id):
-        super().__init__(_id)
-    
-    def SelectAction(self,actions,game_state):
-        return actions[0]
